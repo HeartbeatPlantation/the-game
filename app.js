39616957a9891d4800359e69d2fc828781818f88
HeartbeatPlantation <maximg@hotmail.ch>
2020-01-31 11:40:08 +0100

    set up app.js and add a ejs landing page

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// hello world
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
